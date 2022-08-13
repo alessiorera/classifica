6021d7d77ca0a5ab8418df2d2b7a15f20444fe52
alessiorera <74310263+alessiorera@users.noreply.github.com>
2022-08-13 17:06:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
-var link="https://raw.githubusercontent.com/alessiorera/classifica/main/dati.json"
-var dati={}
-
+var link="https://raw.githubusercontent.com/alessiorera/classifica/main/dati.json";
+var dati={};
 const carica=()=>{
     $.getJSON(link,(data)=>{
-        dati=data
+        dati=data;
         data[0].campionati.forEach(camp => {
-            $("#selCamp").append(`<option>${camp.nome}</option>`)
+            $("#selCamp").append(`<option>${camp.nome}</option>`);
         });
-        aggiornaClass()
-    })
+        aggiornaClass();
+    });
 }
-
 const aggiornaClass=()=>{
-    $("#posizioni").empty()
-    $("#squadre").empty()
+    $("#posizioni").empty();
+    $("#squadre").empty();
     dati[0].campionati.forEach(camp => {
         if(camp.nome==$("#selCamp").val()){
-            var cont=0
+            var cont=0;
             camp.squadre.forEach(squad => {
-                cont++
-                $("#posizioni").append(`<div>${cont}</div>`)
-                $("#squadre").append(`<div><span>${squad.nome}</span><div><i class="fa-solid fa-angle-up" onclick="squadraSu(event)"></i><i class="fa-solid fa-angle-down" onclick="squadraGiu(event)"></i></div></div>`)
-            })
+                cont++;
+                $("#posizioni").append(`<div>${cont}</div>`);
+                $("#squadre").append(`<div><span>${squad.nome}</span><div><i class="fa-solid fa-angle-up" onclick="squadraSu(event)"></i><i class="fa-solid fa-angle-down" onclick="squadraGiu(event)"></i></div></div>`);
+            });
         }
     });
 }
-
 const squadraSu=(e)=>{
-    let squadra=$(e.target).parent().parent()
-    let squadra2=$(e.target).parent().parent().prev()
-    // squadra.css( "background-color", "red" );
-    // squadra2.css( "background-color", "blue" );
+    let squadra=$(e.target).parent().parent();
+    let squadra2=$(e.target).parent().parent().prev();
     squadra.after(squadra2);
 }
-
 const squadraGiu=(e)=>{
-    let squadra=$(e.target).parent().parent()
-    let squadra2=$(e.target).parent().parent().next()
+    let squadra=$(e.target).parent().parent();
+    let squadra2=$(e.target).parent().parent().next();
     squadra.before(squadra2);
 }
-
 const hideShowBtn=()=>{
     $("i").each((i,el)=>{
-        el.classList.toggle("hide")
+        el.classList.toggle("hide");
     })
 }
